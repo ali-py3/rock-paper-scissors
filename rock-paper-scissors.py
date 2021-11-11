5c9ceedea8aa9ffcce2fa83a356a9cc91bac6b59
mmadreza <mohammd78t@gmail.com>
2021-11-11 20:03:46 +0330

    refactoring

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,22 +1,24 @@
 import random
 
+
 def check_win(user, computer):
     if (user == 'r' and computer == 's') or (user == 's' and computer == 'p') or (user == 'p' and computer == 'r'):
-        return True
+        return print(f"Yay! you won! computer Choice is {computer}")
+    return print(f"You lost! computer Choice is {computer}")
+
 
 def rock_paper_scissors():
-    player = input("What is your choice - 'r' for rock, 's' for scissor, 'p' for paper: ")
-    choices = ['r','s','p']
+    player = input("What is your choice - 'r' for rock, 's' for scissor, 'p' for paper: ").lower()
+    choices = ['r', 's', 'p']
     opponent = random.choice(choices)
 
+    if player not in choices:
+        print('The choice is wrong')
+        return rock_paper_scissors()
 
     if player == opponent:
-        return print(f"Its a Tie! Choice is {opponent}")
+        return print(f"Its a Tie! computer Choice is {opponent}")
+    return check_win(player, opponent)
 
-    if check_win(player, opponent):
-        return print(f"Yay! you won! Choice is {opponent}")
-
-    if check_win(player, opponent) != True:
-        return print(f"You lost! Choice is {opponent}")
 
 rock_paper_scissors()
