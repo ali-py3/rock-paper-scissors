3ab97b8392ea8b7599094e34194b6b9b8f42742f
ali-pzk <67821624+ali-py3@users.noreply.github.com>
2021-11-11 12:33:14 -0500

    Update Game

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,22 +1,38 @@
 import random
 
-def check_win(user, computer):
+
+def check_win_or_lose(user, computer):
+    r = 'rock'
+    p = 'paper'
+    s = 'scissor'
+
     if (user == 'r' and computer == 's') or (user == 's' and computer == 'p') or (user == 'p' and computer == 'r'):
-        return True
+        if computer == 'r':
+            return print(f"Yay! you won! Computer Choice is {r}")
+        elif computer == 'p':
+            return print(f"Yay! you won! Computer Choice is {p}")
+        elif computer == 's':
+            return print(f"Yay! you won! Computer Choice is {s}")
+    if (computer == 'r' and user == 's') or (computer == 's' and user == 'p') or (computer == 'p' and user == 'r'):
+        if user == 'r':
+            return print(f"You lost! Computer Choice is {p}")
+        elif user == 'p':
+            return print(f"You lost! Computer Choice is {s}")
+        elif user == 's':
+            return print(f"You lost! Computer Choice is {r}")
 
 def rock_paper_scissors():
-    player = input("What is your choice - 'r' for rock, 's' for scissor, 'p' for paper: ")
-    choices = ['r','s','p']
+    player = input("What is your choice 'r' for rock, 's' for scissor, 'p' for paper: ").lower()
+    choices = ['r', 's', 'p']
     opponent = random.choice(choices)
 
+    if player not in choices:
+        print('The choice is wrong')
+        return rock_paper_scissors()
 
     if player == opponent:
-        return print(f"Its a Tie! Choice is {opponent}")
+        return print(f"Its a Tie! computer Choice is {opponent}")
+    return check_win_or_lose(player, opponent)
 
-    if check_win(player, opponent):
-        return print(f"Yay! you won! Choice is {opponent}")
-
-    if check_win(player, opponent) != True:
-        return print(f"You lost! Choice is {opponent}")
 
 rock_paper_scissors()
